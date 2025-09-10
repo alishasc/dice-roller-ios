7faeccc7755ff6c4067df8f45524bcac12342210
Alisha Carrington <alishasc2001@icloud.com>
2025-09-10 15:12:16 +0100

    Day 95 project setup

---
+++ DiceRoller/DiceRoller/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Alisha Carrington on 10/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Alisha Carrington on 10/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
